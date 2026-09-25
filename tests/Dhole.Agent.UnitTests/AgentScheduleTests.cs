1888ad2496d4442eb7cd79cc0b25e083b2523eb6
Maurice Lang <106346768+MauLang18@users.noreply.github.com>
2026-09-25 07:30:55 -0600

    test(agent): update schedule tests for extraction profile

--- tests/Dhole.Agent.UnitTests/AgentScheduleTests.cs
+++ tests/Dhole.Agent.UnitTests/AgentScheduleTests.cs
@@ -9,14 +9,14 @@ public sealed class AgentScheduleTests
     public void Create_IntervalWithoutPositiveMinutes_ShouldThrow()
     {
         Assert.ThrowsExactly<ArgumentOutOfRangeException>(() =>
-            AgentSchedule.Create("rates", Guid.NewGuid(), Guid.NewGuid(), null,
+            AgentSchedule.Create("rates", Guid.NewGuid(), Guid.NewGuid(), null, null,
                 AgentScheduleType.Interval, null, 0, null, "America/Costa_Rica", "{}", 3, 120));
     }
 
     [TestMethod]
     public void MarkDispatched_Once_ShouldDeactivateWithoutNextExecution()
     {
-        var schedule = AgentSchedule.Create("once", Guid.NewGuid(), Guid.NewGuid(), null,
+        var schedule = AgentSchedule.Create("once", Guid.NewGuid(), Guid.NewGuid(), null, null,
             AgentScheduleType.Once, null, null, DateTime.UtcNow.AddMinutes(5), "UTC", "{}", 0, 60);
 
         schedule.MarkDispatched(DateTime.UtcNow, null);
